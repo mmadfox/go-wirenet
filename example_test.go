5effa17e8d2a7b82c12c58d405486a44726de9d3
mmadfox <sergey.liskonog@gmail.com>
2020-06-27 15:04:55 +0300

    update example_test.go

--- example_test.go
+++ example_test.go
@@ -8,73 +8,148 @@ import (
 	"log"
 	"net"
 	"sync"
+	"time"
 
 	"github.com/mediabuyerbot/go-wirenet"
 )
 
-func wireServer(addr string, isOkCh chan struct{}) {
-	wire, err := wirenet.Mount(addr, wirenet.WithConnectHook(func(closer io.Closer) {
-		close(isOkCh)
-	}))
-	if err != nil {
-		panic(err)
-	}
-	wire.Stream("ns:download", func(ctx context.Context, stream wirenet.Stream) {
-		defer stream.Close()
-		data := make([]byte, 1024*1024)
-		_, err := stream.ReadFrom(bytes.NewReader(data))
+func ExampleCallFromClient() {
+	addr := fmt.Sprintf("127.0.0.1:%d", randomPort())
+	serverIsOk := make(chan struct{})
+	go func() {
+		wire, err := wirenet.Mount(addr, wirenet.WithConnectHook(func(closer io.Closer) {
+			close(serverIsOk)
+		}))
 		if err != nil {
 			panic(err)
 		}
-	})
-	if err := wire.Connect(); err != nil {
-		panic(err)
-	}
-}
-
-func wireClient(addr string, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	sessCh := make(chan wirenet.Session)
-	wire, err := wirenet.Join(addr, wirenet.WithSessionOpenHook(func(session wirenet.Session) {
-		sessCh <- session
-	}))
-	if err != nil {
-		panic(err)
-	}
-	go func() {
+		wire.Stream("ns:download", func(ctx context.Context, stream wirenet.Stream) {
+			defer stream.Close()
+			// write to stream
+			data := make([]byte, 1024*1024)
+			_, err := stream.ReadFrom(bytes.NewReader(data))
+			if err != nil {
+				panic(err)
+			}
+		})
 		if err := wire.Connect(); err != nil {
 			panic(err)
 		}
 	}()
-	sess := <-sessCh
-	log.Println("open session", sess.ID())
-
-	stream, err := sess.OpenStream("ns:download")
-	if err != nil {
-		panic(err)
-	}
-	defer stream.Close()
-	buf := bytes.NewBuffer(nil)
-	n, err := stream.WriteTo(buf)
-	if err != nil {
-		panic(err)
-	}
-	if int(n) == buf.Len() {
-		log.Println("download OK", sess.ID())
-	}
-}
-
-func ExampleWire() {
-	addr := fmt.Sprintf("127.0.0.1:%d", randomPort())
-	serverIsOk := make(chan struct{})
-	go wireServer(addr, serverIsOk)
 	<-serverIsOk
 
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go wireClient(addr, &wg)
+		go func() {
+			defer wg.Done()
+
+			sessCh := make(chan wirenet.Session)
+			wire, err := wirenet.Join(addr, wirenet.WithSessionOpenHook(func(session wirenet.Session) {
+				sessCh <- session
+			}))
+			if err != nil {
+				panic(err)
+			}
+			go func() {
+				if err := wire.Connect(); err != nil {
+					panic(err)
+				}
+			}()
+			sess := <-sessCh
+			log.Println("open session", sess.ID())
+
+			// call from client
+			stream, err := sess.OpenStream("ns:download")
+			if err != nil {
+				panic(err)
+			}
+			defer stream.Close()
+			buf := bytes.NewBuffer(nil)
+			n, err := stream.WriteTo(buf)
+			if err != nil {
+				panic(err)
+			}
+			if int(n) == buf.Len() {
+				log.Println("download OK", sess.ID())
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Print("OK")
+	// Output: OK
+}
+
+func ExampleCallFromServer() {
+	addr := fmt.Sprintf("127.0.0.1:%d", randomPort())
+	serverIsOk := make(chan struct{})
+	totalStreams := 10
+
+	go func() {
+		wire, err := wirenet.Mount(addr,
+			wirenet.WithConnectHook(func(closer io.Closer) {
+				close(serverIsOk)
+			}))
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			if err := wire.Connect(); err != nil {
+				panic(err)
+			}
+		}()
+		step := 0
+		for step < 4 {
+			time.Sleep(time.Second)
+			step++
+			if len(wire.Sessions()) != totalStreams {
+				continue
+			}
+			break
+		}
+		buf := bytes.NewBuffer(nil)
+		for _, sess := range wire.Sessions() {
+			for _, name := range sess.StreamNames() {
+				stream, err := sess.OpenStream(name)
+				if err != nil {
+					panic(err)
+				}
+				stream.WriteTo(buf)
+			}
+		}
+		if buf.Len() == totalStreams*100 {
+			log.Println("Download success!")
+		}
+	}()
+	<-serverIsOk
+
+	var wg sync.WaitGroup
+	for i := 0; i < totalStreams; i++ {
+		wg.Add(1)
+		go func(cid int) {
+			wire, err := wirenet.Join(addr)
+			if err != nil {
+				panic(err)
+			}
+			streamName := fmt.Sprintf("host%d:download", cid)
+			wire.Stream(streamName, func(ctx context.Context, stream wirenet.Stream) {
+				defer wg.Done()
+
+				data := make([]byte, 100)
+				n, err := stream.ReadFrom(bytes.NewReader(data))
+				if err != nil {
+					log.Printf("[ERROR] %v", err)
+					return
+				}
+				if int(n) == len(data) {
+					log.Printf("[INFO] download ok stream %s, session %s",
+						streamName, stream.Session().ID())
+				}
+			})
+			if err := wire.Connect(); err != nil {
+				panic(err)
+			}
+		}(i)
 	}
 	wg.Wait()
 	fmt.Print("OK")
